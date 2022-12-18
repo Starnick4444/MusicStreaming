3840ab8d2b509fe2fbe0989c8150360dfb1aca18
Starnick <nemes.bence1@gmail.com>
2022-12-18 01:33:49 +0100

    Started the listener loop

--- MusicStreaming/Program.cs
+++ MusicStreaming/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace MusicStreamingServer
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace MusicStreamingServer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TcpListener server = null;
+
+            try
+            {
+                //set TcpListener on port, TODO: make it a config
+                Int32 port = 9155;
+
+                server = new TcpListener(IPAddress.Any, port);
+
+                //starting server
+                server.Start();
+
+                byte[] bytes = new byte[1024];
+                string data = null;
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for Connection...");
+
+                    using TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Client connected!");
+
+                    data = null;
+
+                    //Get stream object for reading and writing
+                    NetworkStream networkStream = client.GetStream();
+
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
         }
     }
 }
